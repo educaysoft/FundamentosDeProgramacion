13aa8c2322bc2b87d5ee98a4b0bdca3c098ff219
educaysoft <educaysoft@gmail.com>
2021-06-19 15:14:20 -0500

    Update MatricesOperacionesBasicas.cpp
    Cree el proceso para visualizar los elementos de la matriz1 y la matriz2

--- MatricesOperacionesBasicas/MatricesOperacionesBasicas.cpp
+++ MatricesOperacionesBasicas/MatricesOperacionesBasicas.cpp
@@ -6,7 +6,25 @@ int main()
   float matriz1[10]={2.3,3.4,5.6,9.8,10.4,15.1,4.6,7.3,1.5,3.5};
   float matriz2[10]={2.3,3.4,5.6,9.8,10.4,15.1,4.6,7.3,1.5,3.5};
   float suma[10],resta[10],producto[10],division[10];
-  
+  //Mostrar las matrices Stalin Francis
+  do{
+     cout<<Matriz1[c];
+     c=c+1;  
+     if(c<10){
+       cout<<" - ";
+     }
+   }while(c<10);
+    cout<<endl;
+    c=0;
+    do{
+     cout<<Matriz2[c];
+     c=c+1;  
+     if(c<10){
+       cout<<" - ";
+     }
+   }while(c<10);
+    cout<<endl;
+ 
   
   
   
