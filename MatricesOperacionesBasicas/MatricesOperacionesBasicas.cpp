6355688a012f33211b5c653bd0b2fe1c377e0808
educaysoft <educaysoft@gmail.com>
2021-06-19 14:40:50 -0500

    Create MatricesOperacionesBasicas.cpp
    Se creó un programa basico que contiene lo necesario para comanzar a trabajar colaborativemente

---
+++ MatricesOperacionesBasicas/MatricesOperacionesBasicas.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int c=0;
+  float matriz1[10]={2.3,3.4,5.6,9.8,10.4,15.1,4.6,7.3,1.5,3.5};
+  float matriz2[10]={2.3,3.4,5.6,9.8,10.4,15.1,4.6,7.3,1.5,3.5};
+  float suma[10],resta[10],producto[10],division[];
+  
+  
+  
+  
+  return 0;
+  
+  
+  
+}
